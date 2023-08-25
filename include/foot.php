a933320a6ef0e5cfd373ccaaab6079bbb146ce2b
emily.tw.straight@gmail.com <emily.tw.straight@gmail.com>
2023-08-25 13:45:45 -0700

    still trying

---
+++ include/foot.php
@@ -0,0 +1,4 @@
+<a href="/" class="button"><p>home</p></a>
+<a href="/irc/" class="button"><p>irc</p></a>
+<a href="/gallery/" class="button"><p>gallery</p></a>
+<a href="/other/" class="button"><p>other shit</p></a>
