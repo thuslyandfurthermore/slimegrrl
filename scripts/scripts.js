23b2af5e67f87da3649bc2bb63e9a37ecd2c691f
emily.tw.straight@gmail.com <emily.tw.straight@gmail.com>
2023-09-21 10:17:03 -0700

    preparationss

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,3 +14,14 @@ function addCurrentTab() {
     }
   }
 }
+
+function getStyle() {
+  
+}
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
